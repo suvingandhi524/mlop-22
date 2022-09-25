69285a4276faa8d9d19f964383135ce750f3fd44
suvin <gandhi.3@iitj.ac.in>
2022-09-25 09:17:41 +0530

    plot the resize the image

--- plot_rescale.py
+++ plot_rescale.py
@@ -25,9 +25,10 @@ import matplotlib.pyplot as plt
 from skimage import data, color
 from skimage.transform import rescale, resize, downscale_local_mean
 
+
 image = color.rgb2gray(data.astronaut())
 
-image_rescaled = rescale(image, 0.25, anti_aliasing=False)
+image_rescaled = rescale(image, 0.5, anti_aliasing=False)
 image_resized = resize(image, (image.shape[0] // 4, image.shape[1] // 4),
                        anti_aliasing=True)
 image_downscaled = downscale_local_mean(image, (4, 3))
@@ -35,6 +36,8 @@ image_downscaled = downscale_local_mean(image, (4, 3))
 fig, axes = plt.subplots(nrows=2, ncols=2)
 
 ax = axes.ravel()
+
+
 
 ax[0].imshow(image, cmap='gray')
 ax[0].set_title("Original image")
@@ -45,6 +48,7 @@ ax[1].set_title("Rescaled image (aliasing)")
 ax[2].imshow(image_resized, cmap='gray')
 ax[2].set_title("Resized image (no aliasing)")
 
+
 ax[3].imshow(image_downscaled, cmap='gray')
 ax[3].set_title("Downscaled image (no aliasing)")
 
@@ -52,3 +56,22 @@ ax[0].set_xlim(0, 512)
 ax[0].set_ylim(512, 0)
 plt.tight_layout()
 plt.show()
+
+
+# Import the Images module from pillow
+from PIL import Image
+
+# Open the image by specifying the image path.
+image_path = "image_name.jpeg"
+image_file = Image.open(image_path)
+
+# the default
+image_file.save("image_name.jpg", quality=95)
+
+# Changing the image resolution using quality parameter
+# Example-1
+image_file.save("image_name2.jpg", quality=25)
+
+# Example-2
+image_file.save("image_name3.jpg", quality=1)
+
